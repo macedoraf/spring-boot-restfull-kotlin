488fc28bfb8ad13ecbe784b0732e3511672f712e
Rafaelmacedo <macedoraf@Rafaelmacedos-MacBook-Pro.local>
2021-09-29 00:53:13 -0300

    try updat

--- src/main/kotlin/br/com/fiap/restfullspringapi/base/BaseController.kt
+++ src/main/kotlin/br/com/fiap/restfullspringapi/base/BaseController.kt
@@ -8,18 +8,23 @@ abstract class BaseController<S> {
     abstract val service: BaseService<S>
 
     @GetMapping
+    @CrossOrigin
     fun getAll(): List<S> = service.list()
 
     @GetMapping("/{id}")
+    @CrossOrigin
     fun findById(@PathVariable("id") id: Long): S? = service.findById(id)
 
     @PostMapping
+    @CrossOrigin
     fun create(@RequestBody entity: S): S = service.create(entity)
 
     @DeleteMapping("/{id}")
+    @CrossOrigin
     fun delete(@PathVariable("id") id: Long) = service.deleteById(id)
 
     @PutMapping("/{id}")
+    @CrossOrigin
     fun update(@RequestBody entity: S, @PathVariable("id") id: Long): S = service.update(entity, id)
 
 }
