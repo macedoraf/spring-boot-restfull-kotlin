5d4327bc0d2df91eb5a5f0d323aac07fa1ab2b78
Rafaelmacedo <macedoraf@Rafaelmacedos-MacBook-Pro.local>
2021-09-29 14:15:03 -0300

    remove data

--- src/main/kotlin/br/com/fiap/restfullspringapi/RestfullSpringApiApplication.kt
+++ src/main/kotlin/br/com/fiap/restfullspringapi/RestfullSpringApiApplication.kt
@@ -3,11 +3,21 @@ package br.com.fiap.restfullspringapi
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 
 @SpringBootApplication
 @EnableAutoConfiguration
-class RestfullSpringApiApplication
+@Controller
+class RestfullSpringApiApplication {
+    @RequestMapping("/")
+    @ResponseBody
+    fun home(): String? {
+        return "Hello World!"
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<RestfullSpringApiApplication>(*args)
