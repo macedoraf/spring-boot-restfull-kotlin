0a92677a53e62fc63fe03d0268564375dcabb765
Rafaelmacedo <macedoraf@Rafaelmacedos-MacBook-Pro.local>
2021-09-29 16:37:11 -0300

    teste

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.5.5"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.5.31"
-	kotlin("plugin.spring") version "1.5.31"
+    id("org.springframework.boot") version "2.5.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.spring") version "1.5.31"
 }
 
 group = "br.com.fiap"
@@ -12,37 +12,37 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	// https://mvnrepository.com/artifact/com.h2database/h2
-	implementation("org.postgresql:postgresql:42.2.23.jre7")
-	implementation("org.mariadb.jdbc:mariadb-java-client:2.7.3")
-	implementation("com.h2database:h2:1.4.200")
-	implementation("com.okta.spring:okta-spring-boot-starter:1.4.0")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.5.4")
-	implementation("org.springframework.boot:spring-boot-starter-mustache:2.5.4")
-	implementation("org.springframework.boot:spring-boot-starter-web:2.5.4")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	developmentOnly("org.springframework.boot:spring-boot-devtools:2.5.4")
-	testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.4")
+    // https://mvnrepository.com/artifact/com.h2database/h2
+    implementation("org.postgresql:postgresql:42.2.23.jre7")
+    implementation("org.mariadb.jdbc:mariadb-java-client:2.7.3")
+    implementation("com.h2database:h2:1.4.200")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.5.4")
+    implementation("org.springframework.boot:spring-boot-starter-mustache:2.5.4")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.5.4")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    developmentOnly("org.springframework.boot:spring-boot-devtools:2.5.4")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.4")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 //tasks.withType<Test> {
