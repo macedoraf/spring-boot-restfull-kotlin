35065bcc398b9762454f4fbfcbc12209dcdd97b1
Rafaelmacedo <macedoraf@Rafaelmacedos-MacBook-Pro.local>
2021-09-30 09:50:32 -0300

    teste

--- src/main/kotlin/br/com/fiap/restfullspringapi/RestfullSpringApiApplication.kt
+++ src/main/kotlin/br/com/fiap/restfullspringapi/RestfullSpringApiApplication.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
@@ -17,12 +18,12 @@ class RestfullSpringApiApplication {
     }
 
 
-    @RequestMapping("/")
-    @ResponseBody
+    @GetMapping("/")
     fun home(): String? {
         return "Hello World!"
     }
 }
+
 fun main(args: Array<String>) {
     runApplication<RestfullSpringApiApplication>(*args)
 }
